6c2e63b487c52b6af20043acee8cb1da9b97eaa2
gilbreab6338 <44478225+gilbreab6338@users.noreply.github.com>
2018-12-03 18:54:05 -0500

    Add files via upload

---
+++ P4LAB_Gilbreath.py
@@ -0,0 +1,15 @@
+# Turtle programs a snowflake.
+# 12/03/18
+# CTI-110 P4LAB - Nested Loops
+# Bailey Gilbreath
+#
+import turtle
+
+for i in range(8):
+    turtle.forward(90)
+    turtle.left(45)
+    turtle.forward(90)
+    turtle.left(135)
+    turtle.forward(90)
+    turtle.left(45)
+    turtle.forward(90)
